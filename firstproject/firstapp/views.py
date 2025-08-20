c21547323de1a198b8d56212562aa79c531351a9
parveen-ml <er.parveen44@gmail.com>
2025-08-20 22:08:19 +0530

    configure template

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 # Create your views here.
 
 def hello_world(request):
@@ -16,4 +17,14 @@ class HelloEthiopia(View):
         return HttpResponse("Hello Ethiopia")
     
 
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Success")
+
+    return render(request, 'index.html', {'form': form})
 
--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -73,20 +73,25 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django_db',
+#         'USER': 'root',
+#         'PASSWORD': '___',
+#         'HOST' : '127.0.0.1',
+#         'PORT' : '3306',
+#         'OPTIONS' : {
+#             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': '___',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
-        'OPTIONS' : {
-            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
