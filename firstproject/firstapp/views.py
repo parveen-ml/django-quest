e871d3ad9b2f4ee8ef4a5b5488e51c9aba2ddb14
parveen-ml <er.parveen44@gmail.com>
2025-08-20 12:26:50 +0530

    create function/class based view

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+
+def hello_world(request):
+    '''
+    This is function based view which is taking a request as a parameter here'''
+    return HttpResponse("Hello world!")
+
+
+class HelloEthiopia(View):
+    '''
+    THis is class base view, having get()'''
+    def get(self, request):
+        return HttpResponse("Hello Ethiopia")
+    
+
+
